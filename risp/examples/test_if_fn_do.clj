333fd3eedaa69dd715954276ce98c86a3d3b6be5
oooooba <fuumin.23@gmail.com>
2018-04-03 20:31:09 +0900

    test: add if special-form tests

--- risp/examples/test_if_fn_do.clj
+++ risp/examples/test_if_fn_do.clj
@@ -1,6 +1,21 @@
 (= (list) ())
 (= (list? (list)) true)
 
+(= (if true 7 8) 7)
+(= (if false 7 8) 8)
+(= (if true (+ 1 7) (+ 1 8)) 8)
+(= (if false (+ 1 7) (+ 1 8)) 9)
+(= (if nil 7 8) 8)
+(= (if 0 7 8) 7)
+(= (if "" 7 8) 7)
+(= (if (list) 7 8) 7)
+(= (if (list 1 2 3) 7 8) 7)
+(= (= (list) nil) false)
+
+(= (if false (+ 1 7)) nil)
+(= (if nil 8 7) 7)
+(= (if true (+ 1 7)) 8)
+
 (= ((fn [a b] (+ b a)) 3 4) 7)
 (= ((fn [] 4)) 4)
 (= ((fn [f x] (f x)) (fn [a] (+ 1 a)) 7) 8)
